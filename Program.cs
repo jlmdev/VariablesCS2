39185b95733d83280ad4253cda60b59b3eaddc6b
Josh Mann <jlmann72@gmail.com>
2021-01-14 15:03:39 -0500

    Added simple outputs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,17 @@ namespace VariablesCS2
         static void Main(string[] args)
         {
             int numberOfCupsOfCoffee = 0;
+
             string fullName = "Josh Mann";
+
+            DateTime today = DateTime.Now;
+
+
+            Console.WriteLine($"Coffee cups/day = {numberOfCupsOfCoffee}");
+            Console.WriteLine($"Name = {fullName}");
+            Console.WriteLine($"Today = {today}");
+            Console.WriteLine("Press any key to end");
+            Console.ReadLine();
 
         }
     }
