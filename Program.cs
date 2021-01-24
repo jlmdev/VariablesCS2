9cf27f09b8deb4aeda5657762131e2f8acfcf419
Josh Mann <jlmann72@gmail.com>
2021-01-24 18:12:32 -0500

    Date and Name variables added

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace VariablesCS2
     {
         static void Main(string[] args)
         {
-            // 
+            // Number of Cups of Coffee
             int numberOfCupsOfCoffee = 4;
             Console.WriteLine(numberOfCupsOfCoffee);
+
+            // Full Name
+            string fullName = "Josh Mann";
+            Console.WriteLine(fullName);
+
+            // Today
+            string today = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(today);
         }
     }
 }
