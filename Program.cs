bc062d6d63e2cadcceffa989e5d2e80340eba6bc
Josh Mann <jlmann72@gmail.com>
2021-01-24 18:54:46 -0500

    Created Name handling method. Testing successful

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ namespace VariablesCS2
 {
     class Program
     {
+        static string InputName(string enteredName)
+        {
+            if (enteredName == "Alice")
+            {
+                return $"Hi, {enteredName}! Welcome Back!";
+
+            }
+            else
+            {
+                return $"Hello, {enteredName}.";
+            }
+        }
         static void Main(string[] args)
         {
             // Number of Cups of Coffee
@@ -17,6 +29,11 @@ namespace VariablesCS2
             // Today
             string today = DateTime.Now.ToString("dd/MM/yyyy");
             Console.WriteLine(today);
+
+            // Get entered name and return a response
+            Console.Write("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine(InputName(userName));
         }
     }
 }
